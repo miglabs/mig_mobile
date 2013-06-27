ca77b6764c6c8d019b3b30fa4551548872bda814
apple <apple@appletekiiMac.local>
2013-06-27 18:16:30 +0800

    add player

--- miglab_mobile/classes/PPlayer/PMusicPlayer.h
+++ miglab_mobile/classes/PPlayer/PMusicPlayer.h
@@ -2,12 +2,36 @@
 //  PMusicPlayer.h
 //  miglab_mobile
 //
-//  Created by pig on 13-6-26.
+//  Created by apple on 13-6-26.
 //  Copyright (c) 2013年 pig. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface PMusicPlayer : NSObject
+
+@property (nonatomic, retain) AVPlayer *avPlayer;
+@property (nonatomic, retain) AVPlayerItem *avPlayerItem;
+@property (nonatomic, retain) id audioMix;
+@property (nonatomic, retain) id volumeMixInput;
+
+@property (nonatomic, retain) NSString *songUrl;
+@property (nonatomic, assign) BOOL playBeginState;
+
+//初始化
+-(BOOL)initPlayer;
+//播放
+-(void)play;
+//暂停
+-(void)pause;
+//停止
+-(void)stop;
+//音量调整
+-(void)setVolume:(float)volume;
+//取得播放时间
+-(NSTimeInterval)playableDuration;
+//当前播放时间
+-(NSTimeInterval)playableCurrentTime;
 
 @end
--- miglab_mobile/classes/PPlayer/PMusicPlayer.m
+++ miglab_mobile/classes/PPlayer/PMusicPlayer.m
@@ -2,12 +2,138 @@
 //  PMusicPlayer.m
 //  miglab_mobile
 //
-//  Created by pig on 13-6-26.
+//  Created by apple on 13-6-26.
 //  Copyright (c) 2013年 pig. All rights reserved.
 //
 
 #import "PMusicPlayer.h"
 
 @implementation PMusicPlayer
+
+@synthesize avPlayer = _avPlayer;
+@synthesize avPlayerItem = _avPlayerItem;
+@synthesize audioMix = _audioMix;
+@synthesize volumeMixInput = _volumeMixInput;
+
+@synthesize songUrl = _songUrl;
+@synthesize playBeginState = _playBeginState;
+
+//初始化
+-(BOOL)initPlayer{
+    
+    NSLog(@"initPlayer...");
+    
+    _playBeginState = NO;
+    
+    if (!_songUrl) {
+        return NO;
+    }
+    
+    NSURL *tempSongUrl = [NSURL URLWithString:_songUrl];
+    AVURLAsset *songAsset = [[AVURLAsset alloc] initWithURL:tempSongUrl options:nil];
+    _avPlayerItem = [AVPlayerItem playerItemWithAsset:songAsset];
+    [_avPlayerItem addObserver:self forKeyPath:@"status" options:0 context:NULL];
+    _avPlayer = [AVPlayer playerWithPlayerItem:_avPlayerItem];
+    
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:_avPlayer];
+    
+    [_avPlayer setAllowsExternalPlayback:YES];
+    
+    return YES;
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
+    
+    if ([keyPath isEqualToString:@"status"]) {
+        if (AVPlayerItemStatusReadyToPlay == _avPlayer.currentItem.status) {
+            [_avPlayer play];
+        }
+    }
+    
+}
+
+//播放
+-(void)play{
+    
+    NSLog(@"play...");
+    
+}
+
+//暂停
+-(void)pause{
+    
+    NSLog(@"pause...");
+    
+    if (_avPlayer) {
+        [_avPlayer pause];
+    }
+    
+}
+
+//停止
+-(void)stop{
+    
+    NSLog(@"stop...");
+    
+}
+
+//音量调整
+-(void)setVolume:(float)volume{
+    
+    NSLog(@"setVolume: %f", volume);
+    
+    NSMutableArray *allAudioParams = [NSMutableArray array];
+    
+    AVMutableAudioMixInputParameters *audioInputParams = [AVMutableAudioMixInputParameters audioMixInputParameters];
+    [audioInputParams setVolume:volume atTime:kCMTimeZero];
+    [audioInputParams setTrackID:1];
+    
+    [allAudioParams addObject:audioInputParams];
+    
+    _audioMix = [AVMutableAudioMix audioMix];
+    [_audioMix setInputParameters:allAudioParams];
+    
+    [_avPlayerItem setAudioMix:_audioMix];//mute the player item
+    
+//    [_avPlayer ]
+}
+
+//取得播放时间
+-(NSTimeInterval)playableDuration{
+    
+    NSLog(@"playableDuration...");
+    
+    AVPlayerItem *item = _avPlayer.currentItem;
+    if (item.status == AVPlayerItemStatusReadyToPlay) {
+        return CMTimeGetSeconds(_avPlayer.currentItem.duration);
+    } else {
+        return (CMTimeGetSeconds(kCMTimeInvalid));
+    }
+    
+    return 0;
+}
+
+//当前播放时间
+-(NSTimeInterval)playableCurrentTime{
+    
+    NSLog(@"playableDuration...");
+    
+    AVPlayerItem *item = _avPlayer.currentItem;
+    if (item.status == AVPlayerItemStatusReadyToPlay) {
+        
+        NSLog(@"_avPlayer.currentItem.currentTime: %f", CMTimeGetSeconds(_avPlayer.currentItem.currentTime));
+        if (!_playBeginState && CMTimeGetSeconds(item.currentTime) == CMTimeGetSeconds(item.duration)) {
+            [_avPlayer pause];
+        }
+        
+        _playBeginState = NO;
+        return CMTimeGetSeconds(item.currentTime);
+        
+    } else {
+        return (CMTimeGetSeconds(kCMTimeInvalid));
+    }
+    
+    return 0;
+}
 
 @end
