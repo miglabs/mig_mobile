0a1a204ea718e8bbb4da62162d487ffd3cef3b88
pig@miglab.com <miglab2012>
2013-06-28 00:29:27 +0800

    add api

---
+++ miglab_mobile/classes/MigLabAPI/PCommonUtil.h
@@ -0,0 +1,13 @@
+//
+//  PCommonUtil.h
+//  miglab_mobile
+//
+//  Created by pig on 13-6-27.
+//  Copyright (c) 2013年 pig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PCommonUtil : NSObject
+
+@end
---
+++ miglab_mobile/classes/MigLabAPI/PCommonUtil.m
@@ -0,0 +1,13 @@
+//
+//  PCommonUtil.m
+//  miglab_mobile
+//
+//  Created by pig on 13-6-27.
+//  Copyright (c) 2013年 pig. All rights reserved.
+//
+
+#import "PCommonUtil.h"
+
+@implementation PCommonUtil
+
+@end
