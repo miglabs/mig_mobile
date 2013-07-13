074845f2057857f859090786966111d9fc279cb8
pig@miglab.com <miglab2012>
2013-07-13 14:46:00 +0800

    change 4 playing

--- miglab_mobile/classes/Play/PlayViewController.m
+++ miglab_mobile/classes/Play/PlayViewController.m
@@ -61,7 +61,8 @@
     float height = [UIScreen mainScreen].bounds.size.height;
     PLog(@"height: %f", height);
     
-    UIImage *defaultBackgroundImage = [UIImage imageWithName:@"cd_pic_0" type:@"png"];
+//    UIImage *defaultBackgroundImage = [UIImage imageWithName:@"cd_pic_0" type:@"png"];
+    UIImage *defaultBackgroundImage = [UIImage imageWithName:@"bg_mask_2" type:@"png"];
     _backgroundEGOImageView = [[EGOImageView alloc] initWithPlaceholderImage:defaultBackgroundImage];
     _backgroundEGOImageView.frame = CGRectMake(0, -20, 320, height + 20);
     [self.view addSubview:_backgroundEGOImageView];
