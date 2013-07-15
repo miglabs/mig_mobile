556797983ca7473783c7d3351cb505ce82bc3e22
apple <apple@appletekiiMac.local>
2013-07-15 16:19:58 +0800

    modify api url

--- miglab_mobile/classes/MigLabAPI/MigLabConfig.h
+++ miglab_mobile/classes/MigLabAPI/MigLabConfig.h
@@ -60,82 +60,82 @@
 #define NotificationNameRegisterFailed                      @"NotificationNameRegisterFailed"
 
 //游客信息
-#define HTTP_GUEST                                          @"http://open.fm.miglab.com/api/guest.fcgi"
+#define HTTP_GUEST                                          [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/guest.fcgi"]
 #define NotificationNameGetGuestSuccess                     @"NotificationNameGetGuestSuccess"
 #define NotificationNameGetGuestFailed                      @"NotificationNameGetGuestFailed"
 
 //更新用户信息
-#define HTTP_UPDATEUSER                                     @"http://open.fm.miglab.com/api/usrupdate.fcgi"
+#define HTTP_UPDATEUSER                                     [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/usrupdate.fcgi"]
 #define NotificationUpdateUserSuccess                       @"NotificationUpdateUserSuccess"
 #define NotificationUpdateUserFailed                        @"NotificationUpdateUserFailed"
 
 //默认推荐歌曲
-#define HTTP_DEFAULTMUSIC                                   @"http://open.fm.miglab.com/api/song.fcgi?token=AAOfv3WG35avZspzKhoeodwv2MFd8zYxOUFENUNCMUFBNjgwMDAyRTI2&uid=10001"
+#define HTTP_DEFAULTMUSIC                                   [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/song.fcgi?token=AAOfv3WG35avZspzKhoeodwv2MFd8zYxOUFENUNCMUFBNjgwMDAyRTI2&uid=10001"]
 #define NotificationNameGetDefaultMusicSuccess              @"NotificationNameGetDefaultMusicSuccess"
 #define NotificationNameGetDefaultMusicFailed               @"NotificationNameGetDefaultMusicFailed"
 
 //添加收藏
-#define HTTP_ADDFAVORITE                                    @"http://open.fm.miglab.com/api/collectsong.fcgi"
+#define HTTP_ADDFAVORITE                                    [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/collectsong.fcgi"]
 #define NotificationNameAddFavoriteSuccess                  @"NotificationNameAddFavoriteSuccess"
 #define NotificationNameAddFavoriteFailed                   @"NotificationNameAddFavoriteFailed"
 
 //添加黑名单
-#define HTTP_ADDBLACKLIST                                   @"http://open.fm.miglab.com/api/hatsong.fcgi"
+#define HTTP_ADDBLACKLIST                                   [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/hatsong.fcgi"]
 #define NotificationNameAddBlacklistSuccess                 @"NotificationNameAddBlacklistSuccess"
 #define NotificationNameAddBlacklistFailed                  @"NotificationNameAddBlacklistFailed"
 
 //赠送歌曲
-#define HTTP_PRESENTMUSIC                                   @"http://open.fm.miglab.com/api/sendsong.fcgi"
+#define HTTP_PRESENTMUSIC                                   [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/sendsong.fcgi"]
 #define NotificationNamePresentMusicSuccess                 @"NotificationNamePresentMusicSuccess"
 #define NotificationNamePresentMusicFailed                  @"NotificationNamePresentMusicFailed"
 
 //分享歌曲
-#define HTTP_SHAREMUSIC                                     @"http://open.fm.miglab.com/api/sharesong.fcgi"
+#define HTTP_SHAREMUSIC                                     [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/sharesong.fcgi"]
 #define NotificationNameShareMusicSuccess                   @"NotificationNameShareMusicSuccess"
 #define NotificationNameShareMusicFailed                    @"NotificationNameShareMusicFailed"
 
 //上传用户本地歌曲信息
-#define HTTP_UPLOADMUSIC                                    @"http://open.fm.miglab.com/api/updatesong.fcgi"
+#define HTTP_UPLOADMUSIC                                    [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/updatesong.fcgi"]
 #define NotificationNameUploadMusicSuccess                  @"NotificationNameUploadMusicSuccess"
 #define NotificationNameUploadMusicFailed                   @"NotificationNameUploadMusicFailed"
 
 //获取附近用户
-#define HTTP_NEARBYUSER                                     @"http://open.fm.miglab.com/api/getusrpos.fcgi"
+#define HTTP_NEARBYUSER                                     [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/getusrpos.fcgi"]
 #define NotificationNameNearbyUserSuccess                   @"NotificationNameNearbyUserSuccess"
 #define NotificationNameNearbyUserFailed                    @"NotificationNameNearbyUserFailed"
 
 //获取用户歌单
-#define HTTP_GETUSERLIST                                    @"http://open.fm.miglab.com/api/getsonglist.fcgi"
+#define HTTP_GETUSERLIST                                    [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/getsonglist.fcgi"]
 #define NotificationNameUserListSuccess                     @"NotificationNameUserListSuccess"
 #define NotificationNameUserListFailed                      @"NotificationNameUserListFailed"
 
 //获取用户正在听的歌曲
-#define HTTP_GETPLAYINGMUSIC                                @"http://open.fm.miglab.com/api/nearsong.fcgi"
+#define HTTP_GETPLAYINGMUSIC                                [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/nearsong.fcgi"]
 #define NotificationNamePlayingMusicSuccess                 @"NotificationNamePlayingMusicSuccess"
 #define NotificationNamePlayingMusicFailed                  @"NotificationNamePlayingMusicFailed"
 
 //获取频道目录
-#define HTTP_GETCHANNEL                                     @"http://open.fm.miglab.com/api/cgi-bin/channel.fcgi"
+#define HTTP_GETCHANNEL                                     [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/cgi-bin/channel.fcgi"]
 #define NotificationNameGetChannelSuccess                   @"NotificationNameGetChannelSuccess"
 #define NotificationNameGetChannelFailed                    @"NotificationNameGetChannelFailed"
 
 //获取频道的歌曲
-#define HTTP_GETCHANNELMUSIC                                @"http://open.fm.miglab.com/api/cgi-bin/channel_song.fcgi"
+#define HTTP_GETCHANNELMUSIC                                [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/cgi-bin/channel_song.fcgi"]
 #define NotificationNameGetChannelMusicSuccess              @"NotificationNameGetChannelMusicSuccess"
 #define NotificationNameGetChannelMusicFailed               @"NotificationNameGetChannelMusicFailed"
 
 //获取心情场景词描述
-#define HTTP_MODESCENE                                      @"http://open.fm.miglab.com/api/cgi-bin/getword.fcgi"
+#define HTTP_MODESCENE                                      [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/cgi-bin/getword.fcgi"]
 #define NotificationNameModeSceneSuccess                    @"NotificationNameModeSceneSuccess"
 #define NotificationNameModeSceneFailed                     @"NotificationNameModeSceneFailed"
 
 //获取心情场景歌曲
-#define HTTP_MODEMUSIC                                      @"http://open.fm.miglab.com/api/cgi-bin/word_song.fcgi"
+#define HTTP_MODEMUSIC                                      [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/cgi-bin/word_song.fcgi"]
 #define NotificationNameModeMusicSuccess                    @"NotificationNameModeMusicSuccess"
 #define NotificationNameModeMusicFailed                     @"NotificationNameModeMusicFailed"
 
 //获取心绪地图
-#define HTTP_MODEMAP                                        @"http://open.fm.miglab.com/api/moodmap.fcgi"
+#define HTTP_MODEMAP                                        [NSString stringWithFormat:@"%@%@",HTTP_API_DOMAIN,@"api/moodmap.fcgi"]
 #define NotificationNameModeMapSuccess                      @"NotificationNameModeMapSuccess"
 #define NotificationNameModeMapFailed                       @"NotificationNameModeMapFailed"
 
