80ed1d0e233f8a681ad2daf28e5d0941cf4260ab
pig@miglab.com <miglab2012>
2013-08-13 22:28:55 +0800

    add music view and pic

---
+++ miglab_mobile/classes/Music/MusicViewController.h
@@ -0,0 +1,13 @@
+//
+//  MusicViewController.h
+//  miglab_mobile
+//
+//  Created by pig on 13-8-13.
+//  Copyright (c) 2013年 pig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicViewController : UIViewController
+
+@end
---
+++ miglab_mobile/classes/Music/MusicViewController.m
@@ -0,0 +1,38 @@
+//
+//  MusicViewController.m
+//  miglab_mobile
+//
+//  Created by pig on 13-8-13.
+//  Copyright (c) 2013年 pig. All rights reserved.
+//
+
+#import "MusicViewController.h"
+
+@interface MusicViewController ()
+
+@end
+
+@implementation MusicViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
