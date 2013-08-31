6182e0d03db42eab9251837e7b57670fc46973cd
pig@miglab.com <miglab2012>
2013-09-01 00:28:05 +0800

    int user gene

--- miglab_mobile/classes/DataStruct/UserGene.m
+++ miglab_mobile/classes/DataStruct/UserGene.m
@@ -15,4 +15,20 @@
 @synthesize mood = _mood;
 @synthesize scene = _scene;
 
+-(id)init{
+    
+    self = [super init];
+    if (self) {
+        _channel = [[Channel alloc] init];
+        _channel.channelId = @"1";
+        _type = [[Type alloc] init];
+        _type.typeid = 1;
+        _mood = [[Mood alloc] init];
+        _mood.typeid = 1;
+        _scene = [[Scene alloc] init];
+        _scene.typeid = 1;
+    }
+    return self;
+}
+
 @end
