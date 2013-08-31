54b2fe8923ad3c4768f0bfb1ec77a028be8dcf6f
pig@miglab.com <miglab2012>
2013-08-31 23:46:57 +0800

    modify music tip

--- miglab_mobile/classes/Music/MusicViewController.h
+++ miglab_mobile/classes/Music/MusicViewController.h
@@ -7,10 +7,21 @@
 //
 
 #import "PlayerViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import "CollectNum.h"
 
-@interface MusicViewController : PlayerViewController<UITableViewDataSource, UITableViewDelegate>
+@interface MusicViewController : PlayerViewController<UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate>
 
 @property (nonatomic, retain) UITableView *bodyTableView;
 @property (nonatomic, retain) NSArray *tableTitles;
+
+//gps定位
+@property (nonatomic, retain) CLLocationManager *locationManager;
+@property (nonatomic, retain) CollectNum *collectNum;
+
+-(void)loadCollectedAndNearNumFromServer:(NSString *)tLocation;
+
+-(void)getCollectAndNearNumFailed:(NSNotification *)tNotification;
+-(void)getCollectAndNearNumSuccess:(NSNotification *)tNotification;
 
 @end
--- miglab_mobile/classes/Music/MusicViewController.m
+++ miglab_mobile/classes/Music/MusicViewController.m
@@ -8,6 +8,7 @@
 
 #import "MusicViewController.h"
 #import "MusicSourceMenuCell.h"
+#import "CollectNum.h"
 
 #import "OnlineViewController.h"
 #import "LikeViewController.h"
@@ -23,13 +24,27 @@
 @synthesize bodyTableView = _bodyTableView;
 @synthesize tableTitles = _tableTitles;
 
+@synthesize locationManager = _locationManager;
+@synthesize collectNum = _collectNum;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getCollectAndNearNumFailed:) name:NotificationNameCollectAndNearNumFailed object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getCollectAndNearNumSuccess:) name:NotificationNameCollectAndNearNumSuccess object:nil];
+        
     }
     return self;
+}
+
+-(void)dealloc{
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NotificationNameCollectAndNearNumFailed object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NotificationNameCollectAndNearNumSuccess object:nil];
+    
 }
 
 - (void)viewDidLoad
@@ -40,7 +55,6 @@
     //body
     _bodyTableView = [[UITableView alloc] init];
     _bodyTableView.frame = CGRectMake(11.5, 45 + 10, 297, kMainScreenHeight - 45 - 10 - 10 - 73 - 10);
-//    _bodyTableView.frame = CGRectMake(11.5, 10, 297, kMainScreenHeight - 45 - 10 - 10 - 73 - 10);
     _bodyTableView.dataSource = self;
     _bodyTableView.delegate = self;
     _bodyTableView.backgroundColor = [UIColor clearColor];
@@ -53,14 +67,20 @@
     _bodyTableView.backgroundView = bodyBgImageView;
     [self.view addSubview:_bodyTableView];
     
-    NSDictionary *dicMenu0 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_online", @"MenuImageName", @"在线推荐", @"MenuText", nil];
-    NSDictionary *dicMenu1 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_like", @"MenuImageName", @"我喜欢的", @"MenuText", nil];
-    NSDictionary *dicMenu2 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_nearby", @"MenuImageName", @"附近的好音乐", @"MenuText", nil];
-    NSDictionary *dicMenu3 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_local", @"MenuImageName", @"本地音乐", @"MenuText", nil];
+    NSDictionary *dicMenu0 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_online", @"MenuImageName", @"在线推荐", @"MenuText", @"0", @"MenuTip", nil];
+    NSDictionary *dicMenu1 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_like", @"MenuImageName", @"我喜欢的", @"MenuText", @"90", @"MenuTip", nil];
+    NSDictionary *dicMenu2 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_nearby", @"MenuImageName", @"附近的好音乐", @"MenuText", @"909", @"MenuTip", nil];
+    NSDictionary *dicMenu3 = [NSDictionary dictionaryWithObjectsAndKeys:@"music_source_menu_local", @"MenuImageName", @"本地音乐", @"MenuText", @"0", @"MenuTip", nil];
     _tableTitles = [NSArray arrayWithObjects:dicMenu0, dicMenu1, dicMenu2, dicMenu3, nil];
     
-    //player
-//    self.playerMenuView.frame = CGRectMake(11.5, kMainScreenHeight - 45 - 73 - 10, 297, 73);
+    //gps
+    _locationManager = [[CLLocationManager alloc] init];
+    if ([CLLocationManager locationServicesEnabled]) {
+        [_locationManager setDelegate:self];
+        [_locationManager setDistanceFilter:kCLDistanceFilterNone];
+        [_locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+        [_locationManager startUpdatingLocation];
+    }
     
 }
 
@@ -70,12 +90,80 @@
     // Dispose of any resources that can be recreated.
 }
 
-//override
--(IBAction)doPlayerAvatar:(id)sender{
+-(void)loadCollectedAndNearNumFromServer:(NSString *)tLocation{
     
-    [super doPlayerAvatar:sender];
-    PLog(@"gene doPlayerAvatar...");
+    PLog(@"loadCollectedAndNearNumFromServer...");
     
+    if ([UserSessionManager GetInstance].isLoggedIn && tLocation) {
+        
+        NSString *userid = [UserSessionManager GetInstance].userid;
+        NSString *accesstoken = [UserSessionManager GetInstance].accesstoken;
+        
+        [self.miglabAPI doCollectAndNearNum:userid token:accesstoken taruid:userid radius:@"1000" pageindex:@"0" pagesize:@"10" location:tLocation];
+        
+    } else {
+        
+        [SVProgressHUD showErrorWithStatus:@"您还未登陆哦～"];
+        
+    }
+    
+}
+
+#pragma notification
+
+-(void)getCollectAndNearNumFailed:(NSNotification *)tNotification{
+    
+    PLog(@"getCollectAndNearNumFailed...");
+    
+}
+
+-(void)getCollectAndNearNumSuccess:(NSNotification *)tNotification{
+    
+    PLog(@"getCollectAndNearNumSuccess...");
+    
+    NSDictionary *result = [tNotification userInfo];
+    _collectNum = [result objectForKey:@"result"];
+    NSString *strCollectNum = [NSString stringWithFormat:@"%d", _collectNum.mynum];
+    NSString *strNearNum = [NSString stringWithFormat:@"%d", _collectNum.nearnum];
+    
+    //collected
+    NSDictionary *dicMenu1 = [_tableTitles objectAtIndex:1];
+    [dicMenu1 setValue:strCollectNum forKey:@"MenuTip"];
+    
+    //near
+    NSDictionary *dicMenu2 = [_tableTitles objectAtIndex:2];
+    [dicMenu2 setValue:strNearNum forKey:@"MenuTip"];
+    
+    [_bodyTableView reloadData];
+    
+}
+
+#pragma CLLocationManagerDelegate
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+    
+    PLog(@"[newLocation description]: %@", [newLocation description]);
+    
+    //取得经纬度
+    CLLocationCoordinate2D coordinate = newLocation.coordinate;
+    CLLocationDegrees gLatitude = coordinate.latitude;
+    CLLocationDegrees GLongitude = coordinate.longitude;
+    NSString *strLatitude = [NSString stringWithFormat:@"%g", gLatitude];
+    NSString *strLongitude = [NSString stringWithFormat:@"%g", GLongitude];
+    NSLog(@"strLatitude: %@, strLongitude: %@", strLatitude, strLongitude);
+    
+    [_locationManager stopUpdatingLocation];
+    
+    NSString *strLocation = [NSString stringWithFormat:@"%@,%@", strLatitude, strLongitude];
+    
+    //
+    [self loadCollectedAndNearNumFromServer:strLocation];
+    
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    PLog(@"locationManager didFailWithError: %@", [error localizedDescription]);
 }
 
 #pragma mark - UITableView delegate
@@ -153,6 +241,14 @@
     NSDictionary *dicMenu = [_tableTitles objectAtIndex:indexPath.row];
     cell.menuImageView.image = [UIImage imageWithName:[dicMenu objectForKey:@"MenuImageName"]];
     cell.lblMenu.text = [dicMenu objectForKey:@"MenuText"];
+    
+    int nMenuTip = [[dicMenu objectForKey:@"MenuTip"] intValue];
+    if (nMenuTip > 0) {
+        cell.lblTipNum.text = [NSString stringWithFormat:@"%d", nMenuTip];
+        cell.lblTipNum.hidden = NO;
+    } else {
+        cell.lblTipNum.hidden = YES;
+    }
     
     NSLog(@"cell.frame.size.height: %f", cell.frame.size.height);
     
