bdeb8c5b6402f394a8d0a93a25588bec2ef19632
pig@miglab.com <miglab2012>
2013-09-01 18:43:46 +0800

    add source edit

---
+++ miglab_mobile/classes/Music/MusicSourceEditMenuView.h
@@ -0,0 +1,13 @@
+//
+//  MusicSourceEditMenuView.h
+//  miglab_mobile
+//
+//  Created by pig on 13-9-1.
+//  Copyright (c) 2013年 pig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicSourceEditMenuView : UIView
+
+@end
