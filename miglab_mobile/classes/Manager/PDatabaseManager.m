1669f9b61da54d3d28e915f25ed52cc140bd2ea7
pig@miglab.com <miglab2012>
2013-09-07 14:54:39 +0800

    modify song datastruct for db

--- miglab_mobile/classes/Manager/PDatabaseManager.m
+++ miglab_mobile/classes/Manager/PDatabaseManager.m
@@ -37,9 +37,12 @@
         _db = [FMDatabase databaseWithPath:dbpath];
         [_db open];
         
+        //user info
         [_db executeUpdate:@"create table if not exists USER_ACCOUNT (username text not null, password text, userid integer, accesstoken text, accounttype integer, logintime integer)"];
         [_db executeUpdate:@"create table if not exists USER_INFO (userid text not null, username text, nickname text, gender integer, type integer, birthday text, location text, age integer, source integer, head text)"];
-        [_db executeUpdate:@"create table if not exists SONG_LOCAL_INFO (songid integer, songname text, artist text, duration text, songurl text, hqurl text, lrcurl text, coverurl text, like text, wordid integer, createtime integer, collectnum integer, commentnum integer, hot integer)"];
+        
+        //song cache
+        [_db executeUpdate:@"create table if not exists SONG_LOCAL_INFO (songid integer, songname text, artist text, pubtime text, album text, duration text, songurl text, hqurl text, lrcurl text, coverurl text, like text, type text, typeid integer, createtime integer, collectnum integer, commentnum integer, hot integer)"];
         [_db executeUpdate:@"create table if not exists SONG_DOWNLOAD_INFO (songid integer, type text, filemaxsize integer)"];
         
         //ipod song
@@ -85,6 +88,8 @@
     song0.artist = @"谢容儿";
     song0.songurl = @"http://umusic.9158.com/2013/07/14/14/14/346630_3cd4dbc8abde417c83cd9261f50bdb4c.mp3";
     song0.coverurl = @"http://upic.9158.com/2013/07/12/12/31/20130712123159_img1008690313.jpg";
+    song0.type = @"chl";
+    song0.tid = 0;
     song0.whereIsTheSong = WhereIsTheSong_IN_CACHE;
     [defaultSongInfoList addObject:song0];
     
@@ -94,6 +99,8 @@
     song1.artist = @"慕容晓晓";
     song1.songurl = @"http://umusic.9158.com/2013/07/06/09/21/314001_a2e7fbfef7bd448e9349a3becfdea19e.mp3";
     song1.coverurl = @"http://upic.9158.com/2013/07/07/05/49/20130707054950_img1008690313.jpg";
+    song1.type = @"chl";
+    song1.tid = 0;
     song1.whereIsTheSong = WhereIsTheSong_IN_CACHE;
     [defaultSongInfoList addObject:song1];
     
@@ -103,6 +110,8 @@
     song2.artist = @"G_voice家族";
     song2.songurl = @"http://umusic.9158.com/2013/06/29/16/35/284711_abbf9d95fcbe42a486e86d4281881e0a.mp3";
     song2.coverurl = @"http://upic.9158.com/2013/07/05/07/16/20130705071624_img1008690313.jpg";
+    song2.type = @"chl";
+    song2.tid = 0;
     song2.whereIsTheSong = WhereIsTheSong_IN_CACHE;
     [defaultSongInfoList addObject:song2];
     
@@ -111,6 +120,8 @@
     song3.songname = @"你是我的眼";
     song3.artist = @"萧煌奇";
     song3.songurl = @"http://umusic.9158.com/2013/06/24/23/40/267654_c281b790308e41d2966b24cf56838c0e.mp3";
+    song3.type = @"chl";
+    song3.tid = 0;
     song3.whereIsTheSong = WhereIsTheSong_IN_CACHE;
     [defaultSongInfoList addObject:song3];
     
@@ -281,7 +292,7 @@
  */
 -(void)insertSongInfo:(Song *)tsong{
     
-    NSString *sql = @"insert into SONG_LOCAL_INFO (songid, songname, artist, duration, songurl, hqurl, lrcurl, coverurl, like, wordid, createtime, collectnum, commentnum, hot) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    NSString *sql = @"insert into SONG_LOCAL_INFO (songid, songname, artist, pubtime, album, duration, songurl, hqurl, lrcurl, coverurl, like, type, typeid, createtime, collectnum, commentnum, hot) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     PLog(@"sql: %@", sql);
     
     [_db open];
@@ -297,21 +308,21 @@
     }
     
     NSNumber *numSongId = [NSNumber numberWithLongLong:tsong.songid];
-    NSNumber *numWordId = [NSNumber numberWithInt:tsong.wordid];
+    NSNumber *numTypeId = [NSNumber numberWithInt:tsong.tid];
     NSDate *nowDate = [NSDate date];
     NSNumber *numCreateTime = [NSNumber numberWithLong:[nowDate timeIntervalSince1970]];
     NSNumber *numCollectNum = [NSNumber numberWithInt:tsong.collectnum];
     NSNumber *numCommentNum = [NSNumber numberWithInt:tsong.commentnum];
     NSNumber *numHot = [NSNumber numberWithLongLong:tsong.hot];
     
-    [_db executeUpdate:sql, numSongId, tsong.songname, tsong.artist, tsong.duration, tsong.songurl, tsong.hqurl, tsong.lrcurl, tsong.coverurl, tsong.like, numWordId, numCreateTime, numCollectNum, numCommentNum, numHot];
+    [_db executeUpdate:sql, numSongId, tsong.songname, tsong.artist, tsong.pubtime, tsong.album, tsong.duration, tsong.songurl, tsong.hqurl, tsong.lrcurl, tsong.coverurl, tsong.like, tsong.type, numTypeId, numCreateTime, numCollectNum, numCommentNum, numHot];
     [_db close];
     
 }
 
 -(void)insertSongInfoList:(NSMutableArray *)tsonginfolist{
     
-    NSString *sql = @"insert into SONG_LOCAL_INFO (songid, songname, artist, duration, songurl, hqurl, lrcurl, coverurl, like, wordid, createtime, collectnum, commentnum, hot) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    NSString *sql = @"insert into SONG_LOCAL_INFO (songid, songname, artist, pubtime, album, duration, songurl, hqurl, lrcurl, coverurl, like, type, typeid, createtime, collectnum, commentnum, hot) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     PLog(@"sql: %@", sql);
     
     [_db open];
@@ -345,14 +356,14 @@
         NSLog(@"i: %d", i);
         
         NSNumber *numSongId = [NSNumber numberWithLongLong:tsong.songid];
-        NSNumber *numWordId = [NSNumber numberWithInt:tsong.wordid];
+        NSNumber *numTypeId = [NSNumber numberWithInt:tsong.tid];
         NSDate *nowDate = [NSDate date];
         NSNumber *numCreateTime = [NSNumber numberWithLong:[nowDate timeIntervalSince1970]];
         NSNumber *numCollectNum = [NSNumber numberWithInt:tsong.collectnum];
         NSNumber *numCommentNum = [NSNumber numberWithInt:tsong.commentnum];
         NSNumber *numHot = [NSNumber numberWithLongLong:tsong.hot];
         
-        [_db executeUpdate:sql, numSongId, tsong.songname, tsong.artist, tsong.duration, tsong.songurl, tsong.hqurl, tsong.lrcurl, tsong.coverurl, tsong.like, numWordId, numCreateTime, numCollectNum, numCommentNum, numHot];
+        [_db executeUpdate:sql, numSongId, tsong.songname, tsong.artist, tsong.pubtime, tsong.album, tsong.duration, tsong.songurl, tsong.hqurl, tsong.lrcurl, tsong.coverurl, tsong.like, tsong.type, numTypeId, numCreateTime, numCollectNum, numCommentNum, numHot];
         
     }
     
@@ -377,13 +388,16 @@
         long long psongid = [rs longLongIntForColumn:@"songid"];
         NSString *psongname = [rs stringForColumn:@"songname"];
         NSString *partist = [rs stringForColumn:@"artist"];
+        NSString *ppubtime = [rs stringForColumn:@"pubtime"];
+        NSString *palbum = [rs stringForColumn:@"album"];
         NSString *pduration = [rs stringForColumn:@"duration"];
         NSString *psongurl = [rs stringForColumn:@"songurl"];
         NSString *phqurl = [rs stringForColumn:@"hqurl"];
         NSString *plrcurl = [rs stringForColumn:@"lrcurl"];
         NSString *pcoverurl = [rs stringForColumn:@"coverurl"];
         NSString *plike = [rs stringForColumn:@"like"];
-        int pwordid = [rs intForColumn:@"wordid"];
+        NSString *ptype = [rs stringForColumn:@"type"];
+        int ptypeid = [rs intForColumn:@"typeid"];
         int pcollectnum = [rs intForColumn:@"collectnum"];
         int pcommentnum = [rs intForColumn:@"commentnum"];
         long long phot = [rs longLongIntForColumn:@"hot"];
@@ -392,13 +406,16 @@
         psong.songid = psongid;
         psong.songname = psongname;
         psong.artist = partist;
+        psong.pubtime = ppubtime;
+        psong.album = palbum;
         psong.duration = pduration;
         psong.songurl = psongurl;
         psong.hqurl = phqurl;
         psong.lrcurl = plrcurl;
         psong.coverurl = pcoverurl;
         psong.like = plike;
-        psong.wordid = pwordid;
+        psong.type = ptype;
+        psong.tid = ptypeid;
         psong.collectnum = pcollectnum;
         psong.commentnum = pcommentnum;
         psong.hot = phot;
