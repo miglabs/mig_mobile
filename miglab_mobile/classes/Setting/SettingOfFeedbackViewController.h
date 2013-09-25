9d3ffe83e96d7be500261a0a7b193a819172c98f
pig@miglab.com <miglab2012>
2013-09-25 21:32:15 +0800

    add feedback

---
+++ miglab_mobile/classes/Setting/SettingOfFeedbackViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingOfFeedbackViewController.h
+//  miglab_mobile
+//
+//  Created by pig on 13-9-25.
+//  Copyright (c) 2013年 pig. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SettingOfFeedbackViewController : BaseViewController
+
+@end
