642ebadc4165d3fde214dfd43d6e7f750c961c80
pig@miglab.com <miglab2012>
2013-10-15 21:10:29 +0800

    modify RootNav view

--- miglab_mobile/classes/Home/RootNavigationMenuView.m
+++ miglab_mobile/classes/Home/RootNavigationMenuView.m
@@ -25,6 +25,70 @@
     return self;
 }
 
+-(id)initRootNavigationMenuView{
+    
+    //适配ios7
+    int topdistance = 0;
+    UIImage *homeNavBg = [UIImage imageWithName:@"home_navigation_bg" type:@"png"];
+    long losVersion = [[UIDevice currentDevice].systemVersion floatValue] * 10000;
+    if (losVersion >= 70000) {
+        topdistance = 20;
+        homeNavBg = [UIImage imageWithName:@"home_navigation_bg_ios7" type:@"png"];
+    }
+    
+    //    self = [super initWithFrame:CGRectMake(0, 0, 320, 44)];
+    self = [super initWithFrame:CGRectMake(0, 0, 320, 44 + topdistance)];
+    if (self) {
+        _bgImageView = [[UIImageView alloc] initWithImage:homeNavBg];
+        _bgImageView.frame = CGRectMake(0, 0, 320, 44 + topdistance);
+        [self addSubview:_bgImageView];
+        
+        UIImage *separatorImage = [UIImage imageWithName:@"home_navigation_separator" type:@"png"];
+        UIImageView *separatorImageView01 = [[UIImageView alloc] initWithImage:separatorImage];
+        separatorImageView01.frame = CGRectMake(106, 0 + topdistance, 1, 43);
+        [self addSubview:separatorImageView01];
+        
+        UIImageView *separatorImageView12 = [[UIImageView alloc] initWithImage:separatorImage];
+        separatorImageView12.frame = CGRectMake(213, 0 + topdistance, 1, 43);
+        [self addSubview:separatorImageView12];
+        
+        UIImage *menuSelImage = [UIImage imageWithName:@"home_navigation_menu_sel" type:@"png"];
+        UIColor *menuSelColor = [UIColor colorWithRed:92.0f/255.0f green:210.0f/255.0f blue:248.0f/255.0f alpha:1.0f];
+        
+        //first
+        _btnMenuFirst = [[UIButton alloc] initWithFrame:CGRectMake(0, 0 + topdistance, 106, 43)];
+        _btnMenuFirst.tag = 100;
+        [_btnMenuFirst setBackgroundImage:menuSelImage forState:UIControlStateSelected];
+        [_btnMenuFirst setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_btnMenuFirst setTitleColor:menuSelColor forState:UIControlStateSelected];
+        [_btnMenuFirst setTitle:@"音乐基因" forState:UIControlStateNormal];
+        _btnMenuFirst.titleLabel.font = [UIFont fontOfApp:17.0f];
+        [self addSubview:_btnMenuFirst];
+        
+        //second
+        _btnMenuSecond = [[UIButton alloc] initWithFrame:CGRectMake(107, 0 + topdistance, 106, 43)];
+        _btnMenuSecond.tag = 101;
+        [_btnMenuSecond setBackgroundImage:menuSelImage forState:UIControlStateSelected];
+        [_btnMenuSecond setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_btnMenuSecond setTitleColor:menuSelColor forState:UIControlStateSelected];
+        [_btnMenuSecond setTitle:@"歌单" forState:UIControlStateNormal];
+        _btnMenuSecond.titleLabel.font = [UIFont fontOfApp:17.0f];
+        [self addSubview:_btnMenuSecond];
+        
+        _btnMenuThird = [[UIButton alloc] initWithFrame:CGRectMake(214, 0 + topdistance,106, 43)];
+        _btnMenuThird.tag = 102;
+        [_btnMenuThird setBackgroundImage:menuSelImage forState:UIControlStateSelected];
+        [_btnMenuThird setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_btnMenuThird setTitleColor:menuSelColor forState:UIControlStateSelected];
+        [_btnMenuThird setTitle:@"歌友" forState:UIControlStateNormal];
+        _btnMenuThird.titleLabel.font = [UIFont fontOfApp:17.0f];
+        [self addSubview:_btnMenuThird];
+        
+    }
+    return self;
+    
+}
+
 -(id)initRootNavigationMenuView:(CGRect)frame{
     
 //    self = [super initWithFrame:CGRectMake(0, 0, 320, 45)];
