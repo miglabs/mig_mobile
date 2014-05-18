52b00beb71df41d13e359cb3782a6e7468103b6e
pig <pig@miglab.com>
2014-05-18 12:15:38 +0800

    add guide image

---
+++ miglab_mobile/classes/Guide/GeneGuideView.m
@@ -0,0 +1,35 @@
+//
+//  GeneGuideView.m
+//  miglab_mobile
+//
+//  Created by pig on 14-5-18.
+//  Copyright (c) 2014年 pig. All rights reserved.
+//
+
+#import "GeneGuideView.h"
+
+@implementation GeneGuideView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        
+        _loginIconImageView = [[UIImageView alloc] init];
+        
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
