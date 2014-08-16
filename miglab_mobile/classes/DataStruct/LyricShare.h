9a9dcaf3b30b4d5887ed8ce49bf94a3b91828713
archer <archer@miglab.com>
2014-08-16 20:16:10 +0800

    add lyricshare data structure

---
+++ miglab_mobile/classes/DataStruct/LyricShare.h
@@ -0,0 +1,21 @@
+//
+//  LyricShare.h
+//  miglab_mobile
+//
+//  Created by Archer_LJ on 14-8-16.
+//  Copyright (c) 2014年 pig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LyricShare : NSObject
+
+@property (nonatomic, retain) NSString* songId;
+@property (nonatomic, retain) NSString* lyric;
+@property (nonatomic, retain) NSString* weather;
+@property (nonatomic, retain) NSString* temprature;
+@property (nonatomic, retain) NSString* address;
+
++(id)initWithNSDictionary:(NSDictionary*)dict;
+
+@end
---
+++ miglab_mobile/classes/DataStruct/LyricShare.m
@@ -0,0 +1,36 @@
+//
+//  LyricShare.m
+//  miglab_mobile
+//
+//  Created by Archer_LJ on 14-8-16.
+//  Copyright (c) 2014年 pig. All rights reserved.
+//
+
+#import "LyricShare.h"
+
+@implementation LyricShare
+
++(id)initWithNSDictionary:(NSDictionary *)dict {
+    
+    LyricShare *ls = nil;
+    
+    @try {
+        
+        if (dict && [dict isKindOfClass:[NSDictionary class]]) {
+            
+            ls = [[LyricShare alloc] init];
+            
+            ls.songId = [dict objectForKey:@"id"];
+            ls.lyric = [dict objectForKey:@"lyric"];
+            ls.weather = [dict objectForKey:@"weather"];
+            ls.temprature = [dict objectForKey:@"temp"];
+            ls.address = [dict objectForKey:@"address"];
+        }
+    }
+    @catch (NSException *exception) {
+        
+        PLog(@"parser lyric share structure failed...");
+    }
+}
+
+@end
