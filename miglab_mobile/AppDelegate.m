d07d12599163f525f56c87dd5c4713e52b834c89
archer <archer@miglab.com>
2014-11-01 22:08:44 +0800

    change push system

--- miglab_mobile/AppDelegate.m
+++ miglab_mobile/AppDelegate.m
@@ -334,7 +334,17 @@
     [self.window makeKeyAndVisible];
     
     //注册device token
-    [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound|UIRemoteNotificationTypeAlert];
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        
+        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes categories:nil];
+        [application registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    }
+    else {
+    
+        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound|UIRemoteNotificationTypeAlert];
+    }
     
     if (launchOptions) {
         
